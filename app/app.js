429417cbb910c48a744acc43fbf3a23d27c964a7
plamenvt <plamenvto@gmail.com>
2017-07-10 18:56:56 +0300

    pug files added

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -25,6 +25,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/public', express.static(__dirname + '/public'));
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
