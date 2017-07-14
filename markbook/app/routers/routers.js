04e288f0f188b17f85e8fce0583b14f206485d37
nadezhda-grozdeva <nadezhda.grozdeva@gmail.com>
2017-07-14 23:44:51 +0300

    Fixed nav for logged/not logged users

--- markbook/app/routers/routers.js
+++ markbook/app/routers/routers.js
@@ -3,7 +3,15 @@ module.exports = (app, data) => {
     const login = require('./auth')(app, data);
     const students = require('./students')(app, data);
 
+    app.get('*', function(req, res, next) {
+        // just use boolean for loggedIn
+        res.locals.loggedIn = (req.user) ? true : false;
+
+        next();
+    });
+
     app.use('/', home);
     app.use('/', login);
     app.use('/', students);
+
 };
--- markbook/public/layout.js
+++ markbook/public/layout.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+    $('ul.nav li.dropdown').hover(function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+    }, function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+    });  
+});
+
 $(document).scroll(function() {
   if (document.body.scrollTop > 40) {
     $('nav.navbar').addClass('bg-gradient');
