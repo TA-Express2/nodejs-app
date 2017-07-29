795c1af08cf9361fae7b28a9ac454889a2e80833
plamenvt <plamenvto@gmail.com>
2017-07-29 20:20:23 +0300

    socket connected

--- markbook/socketio/socketio.js
+++ markbook/socketio/socketio.js
@@ -3,41 +3,12 @@ const io = require('socket.io').listen(server);
 io.clients = [];
 
 io.on('connection', (socket) => {
-        const cookie = socket.handshake.headers.cookie;
-        const uniqueIdentifier = cookie.split(';')[0];
-
-        if (io.clients.indexOf(uniqueIdentifier) === -1) {
-            io.clients.push(uniqueIdentifier);
-        }
-
-        socket.join('online-people');
-
-        socket.broadcast
-            .to('online-people')
-            .emit('users-online', io.clients.length);
-
-        // console.log('A user connected - socket id: ' + socket.id);
-        socket.on('request-users-online', (fn) => {
-            fn(io.clients.length);
-            socket.emit(io.clients.length);
-        });
-
-        socket.on('disconnect', () => {
-            const index = io.clients.indexOf(uniqueIdentifier);
-
-            if (index !== -1) {
-                io.clients.splice(index, 1);
-            }
-
-            socket.leave('online people');
-
-            socket.broadcast
-                .to('online-people')
-                .emit('users-online', io.clients.length);
-
-            // console.log('A user disconnected - socket id: ' + socket.id);
-        });
+    console.log('socket connection started');
+    socket.on('my other event', (data) => {
+        console.log(data);
+        socket.emit('messages', { hello: 'world' });
     });
+});
 
 return Promise.resolve(io);
 };
