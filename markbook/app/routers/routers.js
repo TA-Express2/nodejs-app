84bafe7725adaf677789c3370abfec049794a240
Emilia Pavlova <emilia.pavlova@gmail.com>
2017-07-29 19:35:22 +0300

    fix conflict in routes

--- markbook/app/routers/routers.js
+++ markbook/app/routers/routers.js
@@ -3,15 +3,12 @@ module.exports = (app, data) => {
     const login = require('./auth')(app, data);
     const changePassword = require('./users')(app, data);
     const profile = require('./users')(app, data);
-<<<<<<< HEAD
     const students = require('./students')(app, data);
     const marks = require('./students')(app, data);
     const editMarks = require('./students')(app, data);
     const saveMarks = require('./students')(app, data);
     const teachers = require('./teachers')(app, data);
-=======
     const markbook = require('./markbook')(app, data);
->>>>>>> c0b4d52026813bbbfd724c959cab71de8f4922a6
 
     app.get('*', function(req, res, next) {
         // check if there is user logged in
@@ -26,13 +23,7 @@ module.exports = (app, data) => {
     app.use('/:id', students);
     app.use('/edit/:id', students);
     app.use('/', profile);
-<<<<<<< HEAD
-    app.use('/', marks);
-    app.use('/', editMarks);
-    app.use('/', saveMarks);
     app.use('/', teachers);
-=======
     app.use('/:grade', markbook);
     app.use('/saveMarks', markbook);
->>>>>>> c0b4d52026813bbbfd724c959cab71de8f4922a6
 };
